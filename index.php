c3fef8158ca3d249fb6e09f20f6f9b851d826d10
Godson C. Jnr <39808866+GodsonUI@users.noreply.github.com>
2018-05-31 20:40:52 +0100

    Add files via upload
    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Event Reminder & Scheduling System :: Designed by => Godson C. Jnr</title>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta http-equiv="refresh" content="5;url=login.php">
+
+        <!--     Fonts and icons     -->
+        <!-- <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700" /> -->
+        <link rel="stylesheet" href="css/mine.css" />
+        <link rel="stylesheet" href="font/font-awesome/css/font-awesome.min.css" />
+
+        <!-- Material Kit CSS -->
+        <link rel="stylesheet" href="css/material-kit.css">
+    </head>
+    <body style="height: 40em;">
+    	<div class="container my-auto h-100 d-flex justify-content-center">
+    		<div class="row">
+    			<div class="col-md-4 py-2 d-flex justify-content-center text-center">
+    				<p class="text-right text-dark"></p><i class="fa text-right text-danger fa-4x fa-spin fa-spinner"></i>
+    			</div>
+    		</div>
+    	</div>
+    	 <!--   Core JS Files   -->
+        <script src="js/core/jquery.min.js"></script>
+        <script src="js/core/popper.min.js"></script>
+        <script src="js/bootstrap-material-design.js"></script>
+        <script src="js/plugins/moment.min.js"></script>
+        <script src="js/plugins/bootstrap-selectpicker.js"></script>
+        <script src="js/plugins/bootstrap-tagsinput.js"></script>
+        <script src="js/plugins/jasny-bootstrap.min.js"></script>
+        <script src="js/plugins/jquery.flexisel.js"></script>
+        <script src="js/plugins/bootstrap-datetimepicker.min.js"></script>
+        <script src="js/plugins/nouislider.min.js"></script>
+        <script src="js/material-kit.js"></script>
+    </body>
+</html>
